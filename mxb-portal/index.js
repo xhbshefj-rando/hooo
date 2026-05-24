235435868f2f92df6ec3ddef8a3814961c71262e
xhbshefj-rando <xhbshefj@gmail.com>
2026-05-23 22:47:58 -0400

    Update index.js

--- mxb-portal/index.js
+++ mxb-portal/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
-
 app.use(express.json());
 
 // Mock database storage
